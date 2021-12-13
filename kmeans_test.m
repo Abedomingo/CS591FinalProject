f90579f62e858a9bee6e1ebd6dd4ccc174a3d121
Abraham Dominguez <adominguezhernandez@salud.unm.edu>
2021-12-13 00:22:44 -0700

    Made MATLAB code more dynamic with a labels table, and axis selector

--- kmeans_test.m
+++ kmeans_test.m
@@ -1,21 +1,28 @@
-T = readtable('testdata.csv','NumHeaderLines',1);
+T = readtable('testdata.csv');
 
-X = table2array(T(:,5:17));
+Labels = T.Properties.VariableNames(5:12);
+
+X = table2array(T(:,5:12));
 
 k = 5;
 
 [idx,C] = kmeans(X,k);
 
+vars = [1,2,3];
+
 figure;
-plot3(X(idx==1,13),X(idx==1,12),X(idx==1,11),'r.','MarkerSize',12)
+plot3(X(idx==1,vars(1)),X(idx==1,vars(2)),X(idx==1,vars(3)),'r.','MarkerSize',12)
 hold on
-plot3(X(idx==2,13),X(idx==2,12),X(idx==2,11),'b.','MarkerSize',12)
-plot3(X(idx==3,13),X(idx==3,12),X(idx==3,11),'g.','MarkerSize',12)
-plot3(X(idx==4,13),X(idx==4,12),X(idx==4,11),'c.','MarkerSize',12)
-plot3(X(idx==5,13),X(idx==5,12),X(idx==5,11),'y.','MarkerSize',12)
-plot3(C(:,4),C(:,2),C(:,3),'kx',...
+plot3(X(idx==2,vars(1)),X(idx==2,vars(2)),X(idx==2,vars(3)),'b.','MarkerSize',12)
+plot3(X(idx==3,vars(1)),X(idx==3,vars(2)),X(idx==3,vars(3)),'g.','MarkerSize',12)
+plot3(X(idx==4,vars(1)),X(idx==4,vars(2)),X(idx==4,vars(3)),'c.','MarkerSize',12)
+plot3(X(idx==5,vars(1)),X(idx==5,vars(2)),X(idx==5,vars(3)),'y.','MarkerSize',12)
+plot3(C(:,vars(1)),C(:,vars(2)),C(:,vars(3)),'kx',...
      'MarkerSize',15,'LineWidth',3) 
 legend('Cluster 1','Cluster 2','Cluster 3','Cluster 4','Cluster 5','Centroids',...
        'Location','NW')
+xlabel(Labels(vars(1)))
+ylabel(Labels(vars(2)))
+zlabel(Labels(vars(3)))
 title 'Cluster Assignments and Centroids'
 hold off
